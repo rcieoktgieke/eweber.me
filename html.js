eaf3773a7c75a95063408c20dd5278ce1f4b9550
Eric Weber <eric@weber.lol>
2017-06-06 18:28:50 -0700

    add react-helmet

--- html.js
+++ html.js
@@ -1,20 +1,37 @@
 import React from 'react'
-import { prefixLink } from 'gatsby-helpers'
+import Helmet from 'react-helmet'
+import {prefixLink} from 'gatsby-helpers'
 
 const BUILD_TIME = new Date().getTime()
 
 class Html extends React.Component {
   render () {
+    const head = Helmet.rewind()
+
+    let css
+    if (process.env.NODE_ENV === 'production') {
+      css = (
+        <style
+          dangerouslySetInnerHTML={{
+            __html: require('!raw!./public/styles.css')
+          }}
+        />
+      )
+    }
+
     return (
       <html lang='en'>
         <head>
           <meta charSet='utf-8' />
           <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+          {head.title.toComponent()}
+          {head.meta.toComponent()}
+          {css}
         </head>
         <body>
           <div
             id='react-mount'
-            dangerouslySetInnerHTML={{ __html: this.props.body }}
+            dangerouslySetInnerHTML={{__html: this.props.body}}
           />
           <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
         </body>
