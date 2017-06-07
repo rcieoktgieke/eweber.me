5ad92d6a7b0d2999a06693ec8f22036a93bfb1aa
Eric Weber <eric@weber.lol>
2017-06-06 17:28:29 -0700

    create barebones Gatsby site

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Index extends React.Component {
+  render () {
+    return (
+      <div>
+        <p>Welcome to your Gatsby site.</p>
+      </div>
+    )
+  }
+}
