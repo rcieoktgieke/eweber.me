1170fd6471ad2d2f72a951bf56d4437842d7a3a3
Eric Weber <eric@weber.lol>
2017-06-07 18:14:33 -0700

    add homepage basic content

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Helmet from 'react-helmet'
+import {config} from 'config'
 
 export default class Index extends React.Component {
   render () {
     return (
       <div>
-        <p>Welcome to your Gatsby site.</p>
+        <Helmet title={config.siteTitle + ' - Home'} />
+        <h2>About Eric</h2>
+        <p>Hi there!</p>
+        <h2>Contact Eric</h2>
+        <p>Send me an email at <a href='mailto:contact@eweber.me'>contact@eweber.me</a> or give me a call at (925) 518-2633</p>
+        <h2>LinkedIn</h2>
+        <p>Connect with me on <a href='https://www.linkedin.com/in/eric-weber-b0b868a1/' target='blank'>LinkedIn</a></p>
       </div>
     )
   }
