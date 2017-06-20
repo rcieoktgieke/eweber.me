da555a4240af7a064e730d528cbc056fc138ca95
Eric Weber <eric@weber.lol>
2017-06-19 19:46:10 -0700

    add component-level styling to Ikarus elements

--- components/IkarusGalleryImage/index.js
+++ components/IkarusGalleryImage/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import './style.css'
 
 class IkarusGalleryImage extends React.Component {
   render () {
     const imageSrc = this.props.path + this.props.image['filename']
     return (
-      <div className='galleryThumbDiv'>
+      <div className='galleryThumbDiv' style={{
+        // Each image in the gallery is given a bottom and right margin equal to the image margins specified in the gallery object.
+        // The last image in each row is given no right margin. This is in accordance with the gallery div having no margins around it.
+        marginRight: this.props.lastImage ? 0 : this.props.imageMargins,
+        // The last row of images in the gallery are given no bottom margin. This is in accordance with the gallery div having no margins around it.
+        marginBottom: this.props.lastRow ? 0 : this.props.imageMargins
+      }}>
         <img src={imageSrc} />
       </div>
     )
--- components/IkarusGalleryRow/index.js
+++ components/IkarusGalleryRow/index.js
@@ -5,7 +5,7 @@ class IkarusGalleryRow extends React.Component {
   render () {
     const thumbsPath = '/' + this.props.imagesPath + '/' + this.props.thumbsFolderPath + '/'
     const galleryRow = this.props.rowData.map((image, col) =>
-      <IkarusGalleryImage key={col} image={image} path={thumbsPath} />
+      <IkarusGalleryImage key={col} image={image} path={thumbsPath} lastRow={this.props.lastRow} lastImage={col === this.props.rowData.length - 1} imageMargins={this.props.imageMargins} />
     )
     return (
       <div className='galleryRow'>
