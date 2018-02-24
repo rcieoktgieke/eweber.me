c3fff642775cc66f059c6eaedbb764518d944548
Eric Weber <eric@weber.lol>
2018-02-23 18:04:23 -0800

    add bundle component to load documents asynchronously

---
+++ src/components/Bundle/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+class Bundle extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      mod: null
+    }
+  }
+
+  componentWillMount () {
+    this.load(this.props.load)
+  }
+
+  load (load) {
+    this.setState({
+      mod: null
+    })
+
+    load.then(mod => {
+      const Mod = mod.default
+      this.setState({
+        mod: <Mod />
+      })
+    })
+  }
+
+  render () {
+    return this.state.mod
+  }
+}
+
+export default Bundle
