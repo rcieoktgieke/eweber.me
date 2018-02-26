d54cbef7a8a24cc45aa9cfbe4df8a82e958d6d98
Eric Weber <eric@weber.lol>
2018-02-25 16:34:38 -0800

    update Home page code styling

--- src/scenes/Home/index.jsx
+++ src/scenes/Home/index.jsx
@@ -9,20 +9,11 @@ export default class Index extends React.Component {
         <p>Hi there!</p>
         <h2>Contact Eric</h2>
         <p>
-          Send me an email at
-          <FadingLink link='mailto:contact@eweber.me'
-            > contact@eweber.me <
-          /FadingLink>
-          or give me a call at (925) 518-2633
+          Send me an email at <FadingLink link='mailto:contact@eweber.me'>contact@eweber.me</FadingLink> or give me a call at (925) 518-2633
         </p>
         <h2>LinkedIn</h2>
         <p>
-          Connect with me on
-          <FadingLink
-            link='https://www.linkedin.com/in/eric-weber-b0b868a1/'
-            target='blank'
-            > LinkedIn <
-          /FadingLink>
+          Connect with me on <FadingLink link='https://www.linkedin.com/in/eric-weber-b0b868a1/' target='blank'>LinkedIn</FadingLink>
         </p>
       </div>
     )
