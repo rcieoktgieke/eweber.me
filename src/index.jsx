1899d8fb208ccda2878dc9de09115129246790a3
Eric Weber <eric@weber.lol>
2018-05-18 08:30:50 -0700

    convert site index.jsx spaces to tabs

--- src/index.jsx
+++ src/index.jsx
@@ -5,34 +5,34 @@ import PageTemplate from 'PageTemplate/index.js'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 class Site extends React.Component {
-  render () {
-    return (
-      <div>
-        <Route
-          exact path='/'
-          component={() => (
-            <PageTemplate>
-              <div><Link to='/home'>Helloooooo</Link></div>
-            </PageTemplate>
-          )}
-        />
-        <Route
-          path='/home'
-          component={() => (
-            <PageTemplate>
-              <Bundle load={import('Home/index.jsx')} />
-            </PageTemplate>
-          )}
-        />
-      </div>
-    )
-  }
+	render () {
+		return (
+			<div>
+				<Route
+					exact path='/'
+					component={() => (
+						<PageTemplate>
+							<div><Link to='/home'>Helloooooo</Link></div>
+						</PageTemplate>
+					)}
+				/>
+				<Route
+					path='/home'
+					component={() => (
+						<PageTemplate>
+							<Bundle load={import('Home/index.jsx')} />
+						</PageTemplate>
+					)}
+				/>
+			</div>
+		)
+	}
 }
 
 var page = document.getElementById('app').getAttribute('page')
 ReactDOM.render(
-  <BrowserRouter>
-    <Site page={page} />
-  </BrowserRouter>,
-  document.getElementById('app')
+	<BrowserRouter>
+		<Site page={page} />
+	</BrowserRouter>,
+	document.getElementById('app')
 )
