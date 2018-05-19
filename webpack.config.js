5c0e5b5486daede70d2b3916d31cd6d997f48391
Eric Weber <eric@weber.lol>
2018-05-18 18:06:04 -0700

    convert webpack config spaces to tabs

--- webpack.config.js
+++ webpack.config.js
@@ -7,37 +7,37 @@ var CONFIG_DIR = path.resolve(__dirname, 'config')
 var BUILD_DIR = path.resolve(__dirname, 'build/public')
 
 var config = {
-  entry: SRC_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: '[name].bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader'
-      }
-    ]
-  },
-  resolve: {
-    modules: [
-      path.resolve(__dirname, 'node_modules'),
-      SRC_DIR + '/common',
-      SRC_DIR + '/components',
-      SRC_DIR + '/images',
-      SRC_DIR + '/scenes',
-      CONFIG_DIR
-    ]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'common',
-      filename: 'common.js'
-    }),
-    new CopyWebpackPlugin([{ from: 'src/static/*', to: '', flatten: true }])
-  ]
+	entry: SRC_DIR + '/index.jsx',
+	output: {
+		path: BUILD_DIR,
+		filename: '[name].bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?/,
+				include: SRC_DIR,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	resolve: {
+		modules: [
+			path.resolve(__dirname, 'node_modules'),
+			SRC_DIR + '/common',
+			SRC_DIR + '/components',
+			SRC_DIR + '/images',
+			SRC_DIR + '/scenes',
+			CONFIG_DIR
+		]
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'common',
+			filename: 'common.js'
+		}),
+		new CopyWebpackPlugin([{ from: 'src/static/*', to: '', flatten: true }])
+	]
 }
 
 module.exports = config
