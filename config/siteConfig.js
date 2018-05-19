cf8f2577b1562b4a36b19712fa4263a17438a026
Eric Weber <eric@weber.lol>
2018-05-19 15:00:43 -0700

    convert siteConfig.js spaces to tabs

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -1,27 +1,27 @@
 const siteConfig = {
-  siteTitle: 'eweber.me',
-  menu: [
-    {
-      id: 0,
-      name: 'HOME',
-      link: '/home'
-    },
-    {
-      id: 1,
-      name: 'IKARUS',
-      link: '/ikarus'
-    },
-    {
-      id: 2,
-      name: 'PORTFOLIO',
-      link: '/portfolio'
-    },
-    {
-      id: 3,
-      name: 'RESUM\u00c9',
-      link: '/resume'
-    }
-  ]
+	siteTitle: 'eweber.me',
+	menu: [
+		{
+			id: 0,
+			name: 'HOME',
+			link: '/home'
+		},
+		{
+			id: 1,
+			name: 'IKARUS',
+			link: '/ikarus'
+		},
+		{
+			id: 2,
+			name: 'PORTFOLIO',
+			link: '/portfolio'
+		},
+		{
+			id: 3,
+			name: 'RESUM\u00c9',
+			link: '/resume'
+		}
+	]
 }
 
 export default siteConfig
