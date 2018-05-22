92dd4c9bb7ffaad841237743dd4eb37e67cc2c08
Eric Weber <eric@weber.lol>
2018-05-22 08:07:19 -0700

    load page config into site component

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom'
 import Bundle from 'Bundle/index.jsx'
 import PageTemplate from 'PageTemplate/index.js'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
+import siteConfig from 'siteConfig.js'
 
 class Site extends React.Component {
 	render () {
-		var paths = {
-			index: '/index',
-			home: '/home'
+		var paths = {}
+		for (var pageIdx = 0; pageIdx < siteConfig.pages.length; pageIdx ++) {
+			var page = siteConfig.pages[pageIdx]
+			if (page.path != '') {
+				paths[page.name] = '/' + page.name
+			}
 		}
 		paths[this.props.page] = '/'
 		return (
@@ -17,7 +21,7 @@ class Site extends React.Component {
 					exact path={paths.index}
 					component={() => (
 						<PageTemplate>
-							<div><Link to='/home'>Helloooooo</Link></div>
+							<div><Link to={paths.home}>Helloooooo</Link></div>
 						</PageTemplate>
 					)}
 				/>
