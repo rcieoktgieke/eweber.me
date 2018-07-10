e2908ee04842214bf2101ac07a728ad7b566b0b2
Eric Weber <eric@weber.lol>
2018-07-09 18:00:38 -0700

    convert docker user delay loop to blocking exec

--- docker/dev/artifacts/container_user_setup.sh
+++ docker/dev/artifacts/container_user_setup.sh
@@ -11,3 +11,7 @@ usermod -aG sudo "$DEV_USER"
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 chown -R ${DEV_USER}:${DEV_USER_GROUP} "/home/${DEV_USER}"
 cp /root/.bashrc /home/${DEV_USER}
+
+echo "Adding ssh keys to agent"
+eval "$(ssh-agent -s)"
+find $HOME/.ssh -type f ! -name "*.*" ! -name "config" ! -name "*known_hosts" -exec ssh-add {} \;
